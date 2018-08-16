6d21a50450fbd7c3e64d5a3b02fe0abd8dcc0531
mimizukmsk <enone999@gmail.com>
2018-08-16 10:23:55 +0900

    add index-main-loop

--- index.php
+++ index.php
@@ -1,7 +1,37 @@
 <?php get_header(); ?>
 <div class="container">
   <div class="contents">
-  
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+      <article <?php post_class('kiji-list'); ?>>
+        <a href="<?php the_permalink(); ?>">
+          <!-- 画像を追加 -->
+          <?php if(has_post_thumbnail()): ?>
+            <?php the_post_thumbnail('medium'); ?>
+          <?php else: ?>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.gif" alt="no-img" width="150px">
+            <?php endif; ?>
+          <div class="text">
+            <!-- タイトル -->
+            <h2><?php the_title(); ?></h2>
+            <!-- 投稿日を表示 -->
+            <span class="kiji-date">
+              <i class="fas fa-pencil-alt"></i>
+              <time datetime="<?php echo get_the_date('Y-m-d'); ?>">
+                <?php echo get_the_date(); ?>
+              </time>
+            </span>
+            <!-- カテゴリ -->
+            <?php if (!is_category()): ?>
+              <?php if (has_category()): ?>
+              <span class="cat-data">
+                <?php $postcat=get_the_category(); echo $postcat[0]->name; ?>
+              </span>
+              <?php endif; ?>
+            <?php endif; ?>
+          </div>
+        </a>
+      </article>
+    <?php endwhile; endif; ?>
   </div>
   <?php get_sidebar(); ?>
 </div>
