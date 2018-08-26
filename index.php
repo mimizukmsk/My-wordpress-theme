022b6e7a9408854d057a7b8828daf0d2fbdcd141
mimizukmsk <enone999@gmail.com>
2018-08-26 23:06:30 +0900

    index.php fixing finished

--- index.php
+++ index.php
@@ -1,46 +1,45 @@
 <?php get_header(); ?>
 <div class="container container--index">
   <div class="box box--contents">
-  <!-- カテゴリ・タグ一覧ページにタイトルを付与 -->
-  <?php if(is_category() || is_tag()): ?>
-    <h1><i class="fas fa-caret-down"></i> Posts of <?php single_cat_title() ?></h1>
-  <?php elseif(is_year()): ?>
-    <h1><i class="fas fa-caret-down"></i> Posts of <?php the_time("Y") ?>'s</h1>
-  <?php elseif(is_month()): ?>
-    <h1><i class="fas fa-caret-down"></i> Posts of <?php the_time("Y/m") ?>'s</h1>
-  <?php endif ?>
+    <!-- カテゴリ・タグ一覧ページにタイトルを付与 -->
+    <?php if(is_category() || is_tag()): ?>
+    <h1 class="list-title">
+      <i class="fas fa-caret-down icon"></i> Posts of <?php single_cat_title() ?>
+    </h1>
+    <?php elseif(is_month()): ?>
+    <h1 class="list-title">
+      <i class="fas fa-caret-down icon"></i> Posts of <?php the_time("Y/m") ?>'s</h1>
+    <?php endif ?>
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
-      <article <?php post_class('kiji-list'); ?>>
-        <a href="<?php the_permalink(); ?>">
-          <!-- 画像を追加 -->
-          <?php if(has_post_thumbnail()): ?>
-            <?php the_post_thumbnail('medium'); ?>
-          <?php else: ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.gif" alt="no-img">
-            <?php endif; ?>
-          <div class="text">
-            <!-- タイトル -->
-            <h2><?php the_title(); ?></h2>
-            <!-- 投稿日を表示 -->
-            <span class="kiji-date">
-              <i class="fas fa-pencil-alt"></i>
-              <time datetime="<?php echo get_the_date('Y-m-d'); ?>">
-                <?php echo get_the_date(); ?>
-              </time>
-            </span>
-            <!-- カテゴリ -->
-            <?php if (!is_category()): ?>
-              <?php if (has_category()): ?>
-              <span class="cat-data">
-                <?php $postcat=get_the_category(); echo $postcat[0]->name; ?>
-              </span>
-              <?php endif; ?>
-            <?php endif; ?>
-            <!-- 抜粋 -->
-            <?php the_excerpt(); ?>
-          </div>
-        </a>
-      </article>
+    <article <?php post_class( 'post-list'); ?>>
+      <a class="post-list__link" href="<?php the_permalink(); ?>">
+        <!-- 画像を追加 -->
+        <?php if(has_post_thumbnail()): ?>
+          <?php the_post_thumbnail(); ?>
+        <?php else: ?>
+        <img class="wp-post-image" src="<?php echo get_template_directory_uri(); ?>/images/no-image.gif" alt="no-img">
+        <?php endif; ?>
+        <section class="post-list__index">
+          <!-- タイトル -->
+          <h2 class="index__title"><?php the_title(); ?></h2>
+          <!-- 投稿日を表示 -->
+          <span class="index__posted-at">
+            <i class="fas fa-pencil-alt icon"></i>
+            <time datetime="<?php echo get_the_date('Y-m-d'); ?>">
+              <?php echo get_the_date(); ?>
+            </time>
+          </span>
+          <!-- カテゴリ -->
+          <?php if (has_category()): ?>
+          <span class="index__category">
+            <?php $postcat=get_the_category(); echo $postcat[0]->name; ?>
+          </span>
+          <?php endif; ?>
+          <!-- 抜粋 -->
+          <p class="index__excerpt"><?php echo get_the_excerpt(); ?></p>
+        </section>
+      </a>
+    </article>
     <?php endwhile; endif; ?>
     <!-- ページネーション -->
     <div class="pagination">
